24bf3f5ee61b786aca22d16b80d958e57c5b51eb
Murilo-MRS <murilosantana1993@gmail.com>
2022-10-07 16:55:29 -0300

    requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 class Login extends React.Component {
   state = {
     email: '',
     name: '',
+    playButton: false,
+  };
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.fetchToken();
+    this.setState({ playButton: true });
+  };
+
+  fetchToken = async () => {
+    const response = await fetch('https://opentdb.com/api_token.php?command=request');
+    const result = await response.json();
+    const token = await result.token;
+
+    localStorage.setItem('token', token);
   };
 
   render() {
-    const { email, name } = this.state;
+    const { email, name, playButton } = this.state;
     const validateEmail = /^[a-z0-9.!#$%&’+/=?^_`{|}~-]+@[a-z0-9-]+(?:.[a-z0-9-]+)$/;
     const lengthMin = 1;
     const validate = (validateEmail.test(email) && name.length >= lengthMin);
@@ -33,13 +50,18 @@ class Login extends React.Component {
           type="button"
           data-testid="btn-play"
           disabled={ !validate }
+          onClick={ this.handleClick }
         >
           Play
-
         </button>
+        {playButton && <Redirect to="/trivia" />}
       </div>
     );
   }
 }
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  fetchToken: () => dispatch(fetchApiAction()),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
---
+++ src/redux/actions/index.js
@@ -0,0 +1,15 @@
+// export const REQUEST_API = 'REQUEST_API';
+// export const RECEIVE_API = 'RECEIVE_API';
+// // export const FAILED_REQUEST = 'FAILED_REQUEST';
+// const url =
+// function getToken(payload) {
+//   return { type: RECEIVE_API, payload };
+// }
+
+// export const fetchApiAction = () => async (dispatch) => {
+//   const response = await fetch(url);
+
+//   const result = await response.json();
+
+//   dispatch(getToken(result));
+// };
