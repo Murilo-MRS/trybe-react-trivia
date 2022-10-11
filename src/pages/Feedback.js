5dcde06d20fc1720a1c2fa04315153158cb03b5d
Murilo-MRS <murilosantana1993@gmail.com>
2022-10-11 17:12:47 -0300

    [GROUP 16] requisito 12 feedback header

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1 data-testid="feedback-text">Feedback</h1>
       </div>
     );
